84d64920a5883076ae642d790db7363673c6a487
67408 <67408@edu.campusvejle.dk>
2025-09-25 10:34:45 +0200

    N

---
+++ 10_gui/S2010_gui_exercise1.py
@@ -0,0 +1,16 @@
+"""
+Opgave "GUI step 1":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+--------
+
+Bruge det, du har lært i GUI-eksempelfilerne, og byg den GUI, der er afbildet i images/gui_2010.png
+
+--------
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+"""
+
